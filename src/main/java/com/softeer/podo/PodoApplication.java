9a66d113c51879f540f256aa7fe0b12661d66d2b
eckrin <bsh51515@naver.com>
2024-07-23 10:25:20 +0900

    init: initial commit

---
+++ src/main/java/com/softeer/podo/PodoApplication.java
@@ -0,0 +1,13 @@
+package com.softeer.podo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softeer/podo/PodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softeer.podo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
