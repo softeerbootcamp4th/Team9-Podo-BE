22f5e128717080da3f8c7225bcbeaede23039328
eckrin <bsh51515@naver.com>
2024-07-25 14:43:51 +0900

    fix: 불필요한 @Email 검증 제거

--- src/main/java/com/softeer/podo/test/model/dto/TestSignUpRequestDto.java
+++ src/main/java/com/softeer/podo/test/model/dto/TestSignUpRequestDto.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Getter
@@ -13,7 +12,6 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TestSignUpRequestDto {
-    @Email
     @NotBlank
     private String name;
     @NotBlank
