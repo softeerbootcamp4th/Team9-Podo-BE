649e6f5e3720b8f43aae2c577dff51e4a4ed423c
eckrin <bsh51515@naver.com>
2024-07-25 10:13:32 +0900

    feat: 인증필터 테스트용 도메인 추가

---
+++ src/main/java/com/softeer/podo/common/entity/DateEntity.java
@@ -0,0 +1,24 @@
+package com.softeer.podo.common.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class DateEntity {
+    @CreatedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/softeer/podo/user/model/entity/TestUser.java
@@ -0,0 +1,26 @@
+package com.softeer.podo.user.model.entity;
+
+import com.softeer.podo.common.entity.DateEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "testusers")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestUser extends DateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name ="testuser_id")
+    private Long id; //고유 pk
+
+    private String name;
+    private String phoneNum;
+    private Role role;
+}
