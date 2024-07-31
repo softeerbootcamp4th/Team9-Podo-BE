be597c0903538df81abdfa1c8df37f592cfeadac
eckrin <bsh51515@naver.com>
2024-07-31 16:41:30 +0900

    fix: 토큰 발행시 중복검사 수행하지 않도록 수정

--- src/main/java/com/softeer/podo/verification/facade/VerificationFacade.java
+++ src/main/java/com/softeer/podo/verification/facade/VerificationFacade.java
@@ -3,6 +3,7 @@ package com.softeer.podo.verification.facade;
 import com.softeer.podo.security.jwt.TokenInfo;
 import com.softeer.podo.security.jwt.TokenProvider;
 import com.softeer.podo.user.model.dto.UserDto;
+import com.softeer.podo.user.model.entity.Role;
 import com.softeer.podo.user.service.UserService;
 import com.softeer.podo.verification.exception.DuplicatePhoneNumException;
 import com.softeer.podo.verification.exception.TokenNotMatchException;
@@ -28,9 +29,6 @@ public class VerificationFacade {
 
     @Transactional
     public void claimVerificationCode(ClaimVerificationRequestDto dto) {
-        if(userService.checkUserAlreadyExists(dto.getPhoneNum())) {
-            throw new DuplicatePhoneNumException("이미 응모한 전화번호입니다.");
-        }
         String createdCode = verificationService.createAndSaveCode(dto.getName(), dto.getPhoneNum());
 //        messageService.sendVerificationMessage(dto.getPhoneNum(), createdCode);
         log.info("created code for {} = {}", dto.getName(), createdCode);
@@ -38,12 +36,8 @@ public class VerificationFacade {
 
     @Transactional
     public CheckVerificationResponseDto checkVerification(CheckVerificationRequestDto dto) {
-        if(userService.checkUserAlreadyExists(dto.getPhoneNum())) {
-            throw new DuplicatePhoneNumException("이미 응모한 전화번호입니다.");
-        }
         if(verificationService.getAuthInfo(dto.getName(), dto.getPhoneNum(), dto.getVerificationCode())) {
-            UserDto savedUser = userService.saveUser(dto.getName(), dto.getPhoneNum());
-            TokenInfo token = tokenProvider.createAccessToken(savedUser.getName(), savedUser.getPhoneNum(), savedUser.getRole());
+            TokenInfo token = tokenProvider.createAccessToken(dto.getName(), dto.getPhoneNum(), Role.ROLE_USER);
 
             return new CheckVerificationResponseDto(
                     token.getToken(), token.getExpireTime()
