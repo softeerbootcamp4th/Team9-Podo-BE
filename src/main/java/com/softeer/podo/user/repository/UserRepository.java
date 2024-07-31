2ed27ce024d4c5965739bf1d52dfe5c2a42ac247
eckrin <bsh51515@naver.com>
2024-07-31 12:21:45 +0900

    fix: 불필요한 어노테이션 제거

--- src/main/java/com/softeer/podo/user/repository/UserRepository.java
+++ src/main/java/com/softeer/podo/user/repository/UserRepository.java
@@ -2,13 +2,8 @@ package com.softeer.podo.user.repository;
 
 import com.softeer.podo.user.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
-
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByNameAndPhoneNum(String name, String phoneNum);
-    boolean existsByphoneNum(String phoneNum);
+    boolean existsByPhoneNum(String phoneNum);
 }
