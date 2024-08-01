4e79e6caeb72ca912049810a4793adcd2f9a588b
luna156 <v010309@naver.com>
2024-08-01 14:36:11 +0900

    refactor: 필요없은 test entity, repository 제거
    - test 내용은 프론트에 넣기때문에 해당 entity 및 repository 제거

--- src/main/java/com/softeer/podo/event/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.softeer.podo.event.repository;
-
-import com.softeer.podo.event.model.entity.Test;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<Test, Long> {
-}
