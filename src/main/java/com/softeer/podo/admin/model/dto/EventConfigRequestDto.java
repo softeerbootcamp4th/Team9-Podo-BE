d41def2aee6a58554a2cbd22e975801996bae455
luna156 <v010309@naver.com>
2024-08-03 15:04:49 +0900

    feat: event 수정용 request dto 추가

---
+++ src/main/java/com/softeer/podo/admin/model/dto/EventConfigRequestDto.java
@@ -0,0 +1,23 @@
+package com.softeer.podo.admin.model.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventConfigRequestDto {
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime startAt;
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private LocalDateTime endAt;
+	private String repeatDay;
+	@JsonFormat(pattern = "HH:mm:ss")
+	private LocalDateTime repeatTime;
+
+}
