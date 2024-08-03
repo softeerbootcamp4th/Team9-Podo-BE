0d4b31b914d0aa638cd2c54a4d9424caaf3b78db
luna156 <v010309@naver.com>
2024-08-03 14:22:18 +0900

    feat: UserMapper 추가
    - userlist를 userlistdto로 변경하는 mapper 추가

---
+++ src/main/java/com/softeer/podo/admin/model/dto/mapper/UserMapper.java
@@ -0,0 +1,31 @@
+package com.softeer.podo.admin.model.dto.mapper;
+
+
+import com.softeer.podo.admin.model.dto.ArrivalUserDto;
+import com.softeer.podo.admin.model.dto.ArrivalUserListDto;
+import com.softeer.podo.user.model.entity.ArrivalUser;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserMapper {
+
+	public ArrivalUserListDto ArrivalUserListToArrivalUserListDto(List<ArrivalUser> userList) {
+		List<ArrivalUserDto> arrivalUserDtoList = new ArrayList<>();
+		for(ArrivalUser user : userList) {
+			//reward를 제외한 내용 추가
+			arrivalUserDtoList.add(
+					ArrivalUserDto.builder()
+							.id(user.getId())
+							.name(user.getName())
+							.phoneNum(user.getPhoneNum())
+							.rank(user.getArrivalRank())
+							.createdAt(user.getCreatedAt())
+							.build()
+			);
+		}
+		return new ArrivalUserListDto(arrivalUserDtoList);
+	}
+}
