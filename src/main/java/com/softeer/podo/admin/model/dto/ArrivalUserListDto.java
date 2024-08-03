c63777bb63efe6661e76dfd0d215b755ed276c98
luna156 <v010309@naver.com>
2024-08-03 13:47:47 +0900

    fix: ArrivalUserListDto에 생성자 추가

--- src/main/java/com/softeer/podo/admin/model/dto/ArrivalUserListDto.java
+++ src/main/java/com/softeer/podo/admin/model/dto/ArrivalUserListDto.java
@@ -1,7 +1,12 @@
 package com.softeer.podo.admin.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
 public class ArrivalUserListDto {
 	List<ArrivalUserDto> applicationList;
 }
