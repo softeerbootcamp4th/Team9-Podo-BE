4d9ca4776b83c12babb0e0edeb6191f79fa94b54
luna156 <v010309@naver.com>
2024-08-03 13:47:20 +0900

    fix: ArrivalUserDto에 rank 추가

--- src/main/java/com/softeer/podo/admin/model/dto/ArrivalUserDto.java
+++ src/main/java/com/softeer/podo/admin/model/dto/ArrivalUserDto.java
@@ -11,6 +11,7 @@ public class ArrivalUserDto {
 	private Long id;
 	private String name;
 	private String phoneNum;
+	private int rank;
 	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime createdAt;
 	private String reward;
