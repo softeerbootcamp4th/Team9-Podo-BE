18ab873082160d3ddd8251de3a3f1e7575f5e96d
luna156 <v010309@naver.com>
2024-08-05 16:23:30 +0900

    fix: FilterConfig에 url 패턴 추가

--- src/main/java/com/softeer/podo/config/FilterConfig.java
+++ src/main/java/com/softeer/podo/config/FilterConfig.java
@@ -31,6 +31,7 @@ public class FilterConfig {
         FilterRegistrationBean<ExceptionHandleFilter> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new ExceptionHandleFilter(objectMapper));
         registrationBean.addUrlPatterns("/test/auth"); // 필터를 적용할 URL 패턴
+        registrationBean.addUrlPatterns("/v1/*"); // 필터를 적용할 URL 패턴
         registrationBean.setOrder(1); // 필터의 순서 (숫자가 낮을수록 먼저 실행됨)
         return registrationBean;
     }
