cfc033baebc1abd1e099893900c97cb21d178b79
luna156 <v010309@naver.com>
2024-08-08 18:48:15 +0900

    feat: lotsevent dto의 valid 조건 추가
    - LotsApplicationRequestDto에 resultTypeId 범위 체크
    - LotsTypeRequestDto 형식체크 추가

--- src/main/java/com/softeer/podo/event/model/dto/LotsApplicationRequestDto.java
+++ src/main/java/com/softeer/podo/event/model/dto/LotsApplicationRequestDto.java
@@ -1,7 +1,9 @@
 package com.softeer.podo.event.model.dto;
 
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LotsApplicationRequestDto {
-    @NotBlank
+    @Min(value = 1, message = "결과 id가 잘못되었습니다.")
+    @Max(value = 4, message = "결과 id가 잘못되었습니다.")
     private Long resultTypeId;
 }
--- src/main/java/com/softeer/podo/event/model/dto/LotsTypeRequestDto.java
+++ src/main/java/com/softeer/podo/event/model/dto/LotsTypeRequestDto.java
@@ -3,6 +3,7 @@ package com.softeer.podo.event.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,13 +14,17 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LotsTypeRequestDto {
-    @NotBlank
+    @NotBlank(message = "1번 답이 없습니다.")
+    @Pattern(regexp = "^[AB]$", message = "답안 형식에 맞지 않습니다.")
     private String answer1;
-    @NotBlank
+    @NotBlank(message = "2번 답이 없습니다.")
+    @Pattern(regexp = "^[AB]$", message = "답안 형식에 맞지 않습니다.")
     private String answer2;
-    @NotBlank
+    @NotBlank(message = "3번 답이 없습니다.")
+    @Pattern(regexp = "^[AB]$", message = "답안 형식에 맞지 않습니다.")
     private String answer3;
-    @NotBlank
+    @NotBlank(message = "4번 답이 없습니다.")
+    @Pattern(regexp = "^[AB]$", message = "답안 형식에 맞지 않습니다.")
     private String answer4;
 
     @JsonIgnore
