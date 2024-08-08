a7d6496748de655f5dacc0bb7763c5afc5e5e2bb
eckrin <bsh51515@naver.com>
2024-08-08 12:06:04 +0900

    feat: 유형테스트 결과 확인에는 인증/인가 프로세스 제거
    - flow 변경

--- src/main/java/com/softeer/podo/event/controller/EventLotsController.java
+++ src/main/java/com/softeer/podo/event/controller/EventLotsController.java
@@ -1,6 +1,5 @@
 package com.softeer.podo.event.controller;
 
-
 import com.softeer.podo.common.response.CommonResponse;
 import com.softeer.podo.event.model.dto.LotsApplicationRequestDto;
 import com.softeer.podo.event.model.dto.LotsApplicationResponseDto;
@@ -12,28 +11,34 @@ import com.softeer.podo.security.AuthInfo;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequestMapping("/v1/lots")
+@RestController
 @RequiredArgsConstructor
-@Slf4j
 public class EventLotsController {
 
     private final EventLotsService eventLotsService;
 
-    @PostMapping("/application")
-    @Operation(summary = "유형테스트 결과 제출  Api")
-    public CommonResponse<LotsApplicationResponseDto> eventApplication(@Auth AuthInfo authInfo,
-                                                                       @Valid @RequestBody  LotsApplicationRequestDto dto) {
-        return new CommonResponse<>(eventLotsService.applyLotsEvent(authInfo, dto));
-    }
-
     @PostMapping("/comment")
     @Operation(summary = "랜덤추천이벤트 기대평 등록용 Api")
-    public CommonResponse<LotsCommentResponseDto> eventComment(@Auth AuthInfo authInfo,
-                                                               @Valid @RequestBody LotsCommentRequestDto dto) {
-        return new CommonResponse<>(eventLotsService.comment(authInfo, dto));
+    public CommonResponse<LotsCommentResponseDto> eventComment(
+            @Auth AuthInfo authInfo,
+            @Valid @RequestBody LotsCommentRequestDto dto
+    ) {
+        return new CommonResponse<>(eventLotsService.registerComment(authInfo, dto));
     }
+
+    @PostMapping("/application")
+    @Operation(summary = "랜덤추첨 이벤트 응모하기")
+    public CommonResponse<LotsApplicationResponseDto> applyEvent(
+            @Auth AuthInfo authInfo,
+            @Valid @RequestBody LotsApplicationRequestDto dto
+    ) {
+        return new CommonResponse<>(eventLotsService.applyEvent(authInfo, dto));
+    }
+
 }
