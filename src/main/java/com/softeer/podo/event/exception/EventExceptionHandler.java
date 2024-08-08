95ef5f629df239271256d59269b76d8adbaa192c
eckrin <bsh51515@naver.com>
2024-08-08 12:12:17 +0900

    feat: 예외 핸들러 추가
    - 공유 링크가 없을 경우, 암호화 실패, 유형타입 미존재 케이스

--- src/main/java/com/softeer/podo/event/exception/EventExceptionHandler.java
+++ src/main/java/com/softeer/podo/event/exception/EventExceptionHandler.java
@@ -17,28 +17,49 @@ public class EventExceptionHandler {
     @ExceptionHandler(ExistingUserException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public CommonResponse<?> existingPhoneNumbException(ExistingUserException e,  HttpServletRequest request) {
-        log.warn("LOTSAPPLICATION-001> 요청 URI: " + request.getRequestURI() + ", 에러 메세지: " + e.getMessage());
+        log.warn("LOTS-001> 요청 URI: " + request.getRequestURI() + ", 에러 메세지: " + e.getMessage());
         return new CommonResponse<>(ErrorCode.PHONENUM_EXIST_ERROR);
     }
 
     @ExceptionHandler(InvalidSelectionException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public CommonResponse<?> invalidSelectionException(InvalidSelectionException e,  HttpServletRequest request) {
-        log.warn("LOTSAPPLICATION-002> 요청 URI: " + request.getRequestURI() + ", 에러 메세지: " + e.getMessage());
+        log.warn("LOTS-002> 요청 URI: " + request.getRequestURI() + ", 에러 메세지: " + e.getMessage());
         return new CommonResponse<>(ErrorCode.INVALID_SELECTION_ERROR);
     }
 
     @ExceptionHandler(UserNotExistException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public CommonResponse<?> userNotExistException(UserNotExistException e,  HttpServletRequest request) {
-        log.warn("LOTSAPPLICATION-003> 요청 URI: " + request.getRequestURI() + ", 에러 메세지: " + e.getMessage());
+        log.warn("LOTS-003> 요청 URI: " + request.getRequestURI() + ", 에러 메세지: " + e.getMessage());
         return new CommonResponse<>(ErrorCode.USER_NOT_EXIST_ERROR);
     }
 
     @ExceptionHandler(ExistingCommentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public CommonResponse<?> existingCommentException(ExistingCommentException e,  HttpServletRequest request) {
-        log.warn("LOTSAPPLICATION-004> 요청 URI: " + request.getRequestURI() + ", 에러 메세지: " + e.getMessage());
+        log.warn("LOTS-004> 요청 URI: " + request.getRequestURI() + ", 에러 메세지: " + e.getMessage());
         return new CommonResponse<>(ErrorCode.EXISTING_COMMENT_ERROR);
+    }
+
+    @ExceptionHandler(LotsShareLinkNotExistsException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public CommonResponse<?> lotsShareLinkNotExistsException(LotsShareLinkNotExistsException e,  HttpServletRequest request) {
+        log.warn("LOTS-005> 요청 URI: " + request.getRequestURI() + ", 에러 메세지: " + e.getMessage());
+        return new CommonResponse<>(ErrorCode.LOTS_LINK_NOT_EXISTS_ERROR);
+    }
+
+    @ExceptionHandler(AESExecutionException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public CommonResponse<?> AESExecutionException(AESExecutionException e,  HttpServletRequest request) {
+        log.warn("LOTS-006> 요청 URI: " + request.getRequestURI() + ", 에러 메세지: " + e.getMessage());
+        return new CommonResponse<>(ErrorCode.AES_ENC_DEC_ERROR);
+    }
+
+    @ExceptionHandler(InvalidResultTypeException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public CommonResponse<?> invalidResultTypeException(InvalidResultTypeException e,  HttpServletRequest request) {
+        log.warn("LOTS-007> 요청 URI: " + request.getRequestURI() + ", 에러 메세지: " + e.getMessage());
+        return new CommonResponse<>(ErrorCode.INVALID_RESULT_TYPE_ERROR);
     }
 }
---
+++ src/main/java/com/softeer/podo/event/exception/InvalidResultTypeException.java
@@ -0,0 +1,11 @@
+package com.softeer.podo.event.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class InvalidResultTypeException extends RuntimeException {
+    private String message;
+}
